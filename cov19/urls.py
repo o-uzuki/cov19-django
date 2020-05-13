5cc84582c5de57637db1aaf60007b961115e9746
Soichi Tsuzuki <zs2s-tzk@asahi-net.or.jp>
2020-05-13 11:26:06 +0900

    home page

--- cov19/urls.py
+++ cov19/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from cov19diff import views
 
 urlpatterns = [
     path('cov19diff/',include('cov19diff.urls')),
     path('admin/', admin.site.urls),
+    path('',views.doDayly),
 ]
